74caa538058466d16e8028c74064e3e735e17319
June Kim <godlyjune@gmail.com>
2024-07-12 17:40:50 +0900

    Fix an error: empty array response

--- components/details/detailsViewDiagram.tsx
+++ components/details/detailsViewDiagram.tsx
@@ -16,16 +16,16 @@ const View = () => {
   const { data } = useGetEdgarStatement(company.cik);
   const prices = useGetPricesLatest(req);
   const latestPrice = prices.data?.[0].close ?? 0;
-  const cap = latestPrice * (data?.outstandingStock[0].value ?? 0);
-  const net = data?.netIncome[0].value ?? 0;
-  const equ = data?.equity[0].value ?? 0;
+  const cap = latestPrice * (data?.outstandingStock[0]?.value ?? 0);
+  const net = data?.netIncome[0]?.value ?? 0;
+  const equ = data?.equity[0]?.value ?? 0;
 
   return (
     <div className={styles.diagram}>
       <div className={styles.diagramWrapper}>
         <div className={styles.diagramMarketCap}>
           <h5>Market Cap (시가총액)</h5>
-          <span>{shortenNumMillion(cap)}</span>
+          <span>{cap ? shortenNumMillion(cap) : undefined}</span>
         </div>
         <div className={styles.diagramEquity}>
           <h5>Equity (자본)</h5>
@@ -37,15 +37,15 @@ const View = () => {
         </div>
         <div className={styles.diagramPBR}>
           <h5>PBR</h5>
-          <span>{equ ? getRatioString(cap / equ) : undefined}</span>
+          <span>{cap && equ ? getRatioString(cap / equ) : undefined}</span>
         </div>
         <div className={styles.diagramPER}>
           <h5>PER</h5>
-          <span>{net ? getRatioString(cap / net) : undefined}</span>
+          <span>{cap && net ? getRatioString(cap / net) : undefined}</span>
         </div>
         <div className={styles.diagramROE}>
           <h5>ROE</h5>
-          <span>{equ ? `${getRatioString((net / equ) * 100)} %` : undefined}</span>
+          <span>{net && equ ? `${getRatioString((net / equ) * 100)} %` : undefined}</span>
         </div>
       </div>
     </div>
