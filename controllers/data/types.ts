23a3c8ad70f6ebfb44115b3e0910c89498beb850
June Kim <godlyjune@gmail.com>
2024-10-21 18:27:13 +0900

    Add types and states for indicators
    - Rename `ChartOverlay` to `PriceDisplay`
    - Rename `TypeAvgValue` to `TypePriceValue`
    - Add `TypePriceVolumeValue`

--- controllers/data/types.ts
+++ controllers/data/types.ts
@@ -1,15 +1,28 @@
 export type TypeError = { code: number; message: string };
+export type TypeRectCoordi = { x: number; y: number; w: number; h: number };
 
 export type TypePriceRequestType = 'daily' | 'weekly' | 'latest';
 export type TypeAvgMethod = 'simple' | 'exponential' | 'weighted';
-export type TypeAvgValue = 'close' | 'open' | 'high' | 'low';
+export type TypePriceValue = 'close' | 'open' | 'high' | 'low';
+export type TypePriceVolumeValue = TypePriceValue | 'volume';
 export type TypeChart =
   | 'price'
+  | 'mini-price'
+  | 'volume'
   | 'percent-change'
   | 'heikin-aski'
   | 'heikin-aski-smoothed'
-  | 'mini-price'
-  | 'volume';
+  | 'adx'
+  | 'rsi'
+  | 'macd'
+  | 'macd-v'
+  | 'atrp'
+  | 'co'
+  | 'cmf'
+  | 'so';
+export type TypeDartReportCode = '11011' | '11012' | '11013' | '11014';
+export type TypeDartIndexCode = 'M210000' | 'M220000' | 'M230000' | 'M240000';
+export type TypeDartStatementType = 'OFS' | 'CFS';
 
 export type TypeIDWeek = { year: number; week: number };
 export type TypeDate = { date: Date | TypeIDWeek };
@@ -18,14 +31,28 @@ export type TypePriceRequest = { code: string; type: TypePriceRequestType };
 export type TypeChartRequest = { code: string; type: TypeChart };
 export type TypeIDPriceMA = TypePriceRequest & { method: TypeAvgMethod; period: number };
 
+export type TypeChartData =
+  | TypePrice
+  | TypePriceVolume
+  | TypePricePercentChange
+  | TypeParabolicSAR
+  | TypeMovingAvg
+  | TypePriceBollingerBands
+  | TypeAdx
+  | TypeRsi
+  | TypeMacd
+  | TypeMacdV
+  | TypeAtrp
+  | TypeChaikin
+  | TypeStochastic;
 export type TypePrice = TypeDate & {
   open: number;
   close: number;
   high: number;
   low: number;
 };
-export type TypePricePercentChange = TypeDate & { percent_change: number };
 export type TypePriceVolume = TypePrice & { volume: number };
+export type TypePricePercentChange = TypeDate & { percent_change: number };
 export type TypeParabolicSAR = TypeDate & { sar: number; isUpTrend: boolean };
 export type TypeMovingAvg = TypeDate & { avg: number };
 export type TypePriceBollingerBands = TypeDate & {
@@ -33,31 +60,68 @@ export type TypePriceBollingerBands = TypeDate & {
   middle: number;
   lower: number;
 };
+export type TypeAdx = TypeDate & { posDI: number; negDI: number; adx: number };
+export type TypeRsi = TypeDate & { rsi: number };
+export type TypeMacd = TypeDate & { macd: number; signal: number; histogram: number };
+export type TypeMacdV = TypeDate & { macdV: number; signal: number; histogram: number };
+export type TypeAtrp = TypeDate & { atrp: number };
+export type TypeChaikin = TypeDate & { cmf: number; co: number };
+export type TypeStochastic = TypeDate & { fullK: number; fullD: number };
+export type TypePriceRaw = TypePriceVolume & {
+  trading_value: number;
+  base_stock_cnt: number;
+};
 
-export type TypeChartData =
-  | TypePrice
-  | TypePriceVolume
-  | TypePricePercentChange
-  | TypeParabolicSAR
-  | TypeMovingAvg
-  | TypePriceBollingerBands;
-
-export type TypeChartOverlay = 'LatestPrice' | 'ParabolicSAR' | 'BollingerBands';
-export type TypeChartDisplay = {
+export type TypePriceDisplayItem = 'LatestPrice' | 'ParabolicSAR' | 'BollingerBands';
+export type TypeAdxDisplayItem = 'ADX' | 'pDI' | 'nDI' | 'buy' | 'sell' | 'trendConfirm';
+export type TypeRsiDisplayItem = 'overbought' | 'oversold';
+export type TypeMacdDisplayItem = 'MACD' | 'signal' | 'histogram';
+export type TypeMacdVDisplayItem =
+  | 'MACDV'
+  | 'signal'
+  | 'histogram'
+  | 'overbought'
+  | 'oversold'
+  | 'upsideMomentum'
+  | 'downsideMomentum';
+export type TypeCMFDisplayItem = 'noSignalZone';
+export type TypeStochasticDisplayItem =
+  | 'fullK'
+  | 'fullD'
+  | 'overbought'
+  | 'oversold'
+  | 'trendConfirm';
+export type TypePriceDisplay = {
   LatestPrice: boolean;
   ParabolicSAR: boolean;
   BollingerBands: boolean;
 };
-
-export type TypeCompany = {
-  cik: string;
-  code: string;
-  name: string;
+export type TypeAdxDisplay = {
+  ADX: boolean;
+  pDI: boolean;
+  nDI: boolean;
+  buy: boolean;
+  sell: boolean;
+  trendConfirm: boolean;
 };
-export type TypeCompanyTab = {
-  uuid: string;
-  company: TypeCompany;
-  mainType: TypePriceRequestType;
+export type TypeRsiDisplay = { overbought: boolean; oversold: boolean };
+export type TypeMacdDisplay = { MACD: boolean; signal: boolean; histogram: boolean };
+export type TypeMacdVDisplay = {
+  MACDV: boolean;
+  signal: boolean;
+  histogram: boolean;
+  overbought: boolean;
+  oversold: boolean;
+  upsideMomentum: boolean;
+  downsideMomentum: boolean;
+};
+export type TypeCMFDisplay = { noSignalZone: boolean };
+export type TypeStochasticDisplay = {
+  fullK: boolean;
+  fullD: boolean;
+  overbought: boolean;
+  oversold: boolean;
+  trendConfirm: boolean;
 };
 
 export type TypeEdgarStatementResRaw = {
