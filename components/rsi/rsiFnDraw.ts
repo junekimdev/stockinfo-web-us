26b34fb038cc63be88039c04e8fb3a5da1521aea
June Kim <godlyjune@gmail.com>
2024-10-21 18:51:50 +0900

    Add rsi indicator comp

---
+++ components/rsi/rsiFnDraw.ts
@@ -0,0 +1,121 @@
+import * as d3 from 'd3';
+import {
+  addClipPathAsShowWindow,
+  getDateString,
+  getXCentered,
+  initChart,
+} from '../../controllers/chart';
+import { TypeRectCoordi, TypeRsi, TypeRsiDisplay } from '../../controllers/data/types';
+
+const draw = (id: string, data: TypeRsi[], display: TypeRsiDisplay, marginLeft: number) => {
+  if (!data?.length) return;
+  const yMin = 0;
+  const yMax = 100;
+  const margin = {
+    top: 10,
+    bottom: 10,
+    left: marginLeft,
+    right: marginLeft + 10,
+  };
+  const chartHeight = 120;
+  const lineColorOverbought = 'red';
+  const lineColorOversold = 'blue';
+  const lineColorRsi = 'gray';
+  const areaColorOverbought = 'rgba(255,0,0,0.7)';
+  const areaColorOversold = 'rgba(0,0,255,0.7)';
+
+  const overboughtValue = 70;
+  const oversoldValue = 30;
+
+  const { chart, x, y } = initChart({
+    id,
+    yMin,
+    yMax,
+    data,
+    margin,
+    chartHeight,
+    tickYCount: 5,
+  });
+
+  const w = (x(getDateString(data[data.length - 1])) ?? 0) + x.bandwidth();
+
+  // Add clip path
+  const coordiOverbought: TypeRectCoordi = { x: 0, y: 0, w, h: y(overboughtValue) };
+  const coordiOversold: TypeRectCoordi = {
+    x: 0,
+    y: y(oversoldValue),
+    w,
+    h: Math.abs(y(oversoldValue) - y(yMin)),
+  };
+  const clipIdOverbought = addClipPathAsShowWindow(chart, id, coordiOverbought, 'overbought');
+  const clipIdOversold = addClipPathAsShowWindow(chart, id, coordiOversold, 'oversold');
+
+  // Draw lines
+  if (display.overbought) {
+    chart
+      .append('path')
+      .attr('fill', areaColorOverbought)
+      .attr('stroke', 'none')
+      .attr('clip-path', `url(${clipIdOverbought})`)
+      .datum(data)
+      .attr(
+        'd',
+        d3
+          .area<TypeRsi>()
+          .x((d) => getXCentered(d, x))
+          .y1((d) => y(d.rsi))
+          .y0(y(yMin)),
+      );
+
+    const p1: [number, number] = [0, y(overboughtValue)];
+    const p2: [number, number] = [w, y(overboughtValue)];
+    chart
+      .append('path')
+      .attr('fill', 'none')
+      .attr('stroke', lineColorOverbought)
+      .attr('stroke-width', 1)
+      .attr('d', d3.line()([p1, p2]));
+  }
+
+  if (display.oversold) {
+    chart
+      .append('path')
+      .attr('fill', areaColorOversold)
+      .attr('stroke', 'none')
+      .attr('clip-path', `url(${clipIdOversold})`)
+      .datum(data)
+      .attr(
+        'd',
+        d3
+          .area<TypeRsi>()
+          .x((d) => getXCentered(d, x))
+          .y1((d) => y(d.rsi))
+          .y0(y(yMax)),
+      );
+
+    const p1: [number, number] = [0, y(oversoldValue)];
+    const p2: [number, number] = [w, y(oversoldValue)];
+    chart
+      .append('path')
+      .attr('fill', 'none')
+      .attr('stroke', lineColorOversold)
+      .attr('stroke-width', 1)
+      .attr('d', d3.line()([p1, p2]));
+  }
+
+  chart
+    .append('path')
+    .attr('fill', 'none')
+    .attr('stroke', lineColorRsi)
+    .attr('stroke-width', 1)
+    .datum(data)
+    .attr(
+      'd',
+      d3
+        .line<TypeRsi>()
+        .x((d) => getXCentered(d, x))
+        .y((d) => y(d.rsi)),
+    );
+};
+
+export default draw;
---
+++ components/rsi/rsiPresenter.tsx
@@ -0,0 +1,73 @@
+import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { StateRsi, StateRsiDisplay } from '../../controllers/data/states';
+import { TypePriceRequest } from '../../controllers/data/types';
+import { useGetPrices } from '../../controllers/net/price';
+import styles from './rsi.module.scss';
+import draw from './rsiFnDraw';
+import { useDisplayCheckboxChange, useRsi } from './rsiInteractor';
+
+const Presenter = (props: { req: TypePriceRequest; marginLeft: number; max?: number }) => {
+  const { req, marginLeft, max = 120 } = props;
+
+  useRsi(req);
+  const { data } = useGetPrices(req);
+  const dataRsi = useRecoilValue(StateRsi(req));
+  const display = useRecoilValue(StateRsiDisplay);
+
+  const onOversoldLineCheckboxChange = useDisplayCheckboxChange('oversold');
+  const onOverboughtLineCheckboxChange = useDisplayCheckboxChange('overbought');
+
+  const chartTitle = `${req.type} RSI`;
+  const chartID = `${styles.chart}-${req.code}-${req.type}`;
+
+  const overboughtInputID = `${chartID}-overbought`;
+  const oversoldInputID = `${chartID}-oversold`;
+
+  useEffect(() => {
+    if (dataRsi.length) {
+      draw(chartID, dataRsi.slice(-max), display, marginLeft);
+    }
+  }, [chartID, marginLeft, max, data, dataRsi, display]);
+
+  return (
+    <div className={styles.chartContainer}>
+      <div className={styles.chartTitle}>
+        <h5>{chartTitle}</h5>
+        <span
+          className="material-symbols-outlined"
+          title="RSI(Relative Strength Index) is a momentum oscillator developed by John Welles Wilder Jr. : RSI (14)"
+        >
+          info
+        </span>
+        <h5>RSI &lt; 30 : Oversold,</h5>
+        <h5>RSI &gt; 70 : Overbought</h5>
+      </div>
+      <svg id={chartID} className={styles.chart}></svg>
+      <div className={styles.displaySelectorWrapper} onClick={(e) => e.stopPropagation()}>
+        <div className={styles.displaySelector} title="Overbought above 70">
+          <input
+            type="checkbox"
+            name={overboughtInputID}
+            id={overboughtInputID}
+            checked={display.overbought}
+            onChange={onOverboughtLineCheckboxChange}
+          />
+          <label htmlFor={overboughtInputID}>Overbought</label>
+        </div>
+        <div className={styles.displaySelector} title="Oversold below 30">
+          <input
+            type="checkbox"
+            name={oversoldInputID}
+            id={oversoldInputID}
+            checked={display.oversold}
+            onChange={onOversoldLineCheckboxChange}
+          />
+          <label htmlFor={oversoldInputID}>Oversold</label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Presenter;
