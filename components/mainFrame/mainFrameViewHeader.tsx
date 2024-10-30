0ffb869428ed9abde8005c3f89cd2d1ae05b15e3
June Kim <godlyjune@gmail.com>
2024-10-30 16:24:50 +0900

    Stack `code` and `cik` vertically in header

--- components/mainFrame/mainFrameViewHeader.tsx
+++ components/mainFrame/mainFrameViewHeader.tsx
@@ -19,8 +19,10 @@ const View = () => {
     <header className={styles.header}>
       <div className={styles.companyInfo}>
         <h1>{company.name}</h1>
-        <h3>{company.code}</h3>
-        <h3>{company.cik}</h3>
+        <div className={styles.companyInfoSub}>
+          <h3>{company.code}</h3>
+          <h3>{company.cik}</h3>
+        </div>
         <h3>{mainType}</h3>
         <LatestPrice code={company.code} />
       </div>
