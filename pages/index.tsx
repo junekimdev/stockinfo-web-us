432c14a5444e8522a06173c6a309ed0d47a0672f
June Kim <godlyjune@gmail.com>
2025-02-21 17:19:30 +0900

    Make resetting current tab a role of page
    # Pages which reset current tab
    - `/index`
    - `/search`

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,21 @@
+import { useResetAtom } from 'jotai/utils';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 import MainFrame from '../components/mainFrame';
 import Meta from '../components/meta';
 import Search from '../components/search';
+import * as gState from '../controllers/data/states';
 
 const Page = () => {
   const publicUrl = process.env.NEXT_PUBLIC_URL ?? 'localhost:3000';
   const router = useRouter();
+  const resetCurrent = useResetAtom(gState.currentTab);
 
   useEffect(() => {
     window.scrollTo(0, 0);
+    resetCurrent();
     router.prefetch('/chart');
-  }, [router]);
+  }, [resetCurrent, router]);
 
   return (
     <>
